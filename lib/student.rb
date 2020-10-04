bc89ba580614f961a7faae5fadcc960dea9a5943
Pbsmith82 <pbsmith82@gmail.com>
2020-10-04 14:51:43 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(arg)
+        @knowledge << arg
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
