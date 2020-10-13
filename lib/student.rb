3dbaa5da1893d331a0e288b25dfc95ce46fa0c13
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-13 06:58:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "../lib/user.rb"
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end
+
+  def learn(fact)
+    @knowledge << fact
+  end
 
 end
